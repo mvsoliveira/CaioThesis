7c5bf9db47ecbeb2bb9071044150b87b5a3933cf
Marcos Oliveira <marcos.oliveira@cern.ch>
2018-12-21 03:45:24 +0100

    reformatado codigo

--- python/processing.py
+++ python/processing.py
@@ -3,7 +3,6 @@ import copy
 
 
 def parser(filename):
-
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         total_list = list(reader)
@@ -16,34 +15,34 @@ def parser(filename):
     return [cold, coln, values]
 
 
-def is_valid_pair(i,w,l,col,values):
+def is_valid_pair(i, w, l, col, values):
     if values[i][col['m']] != '':
         # it is a pair
-        if i-w >= 0 and i+w < l:
+        if i - w >= 0 and i + w < l:
             # list is not out of range
-            if are_all_the_same(get_values(i, w, w,'temporada',col,values)):
+            if are_all_the_same(get_values(i, w, w, 'temporada', col, values)):
                 # the same season
                 if are_all_the_same(get_values(i, w, w, 'time', col, values)):
                     # the same team
                     if values[i][col['trocou']] == '0':
-                        if are_all_the_same(get_values(i, w, w-1, 'trocou', col, values)):
-                            # if it did not change, should not change in the entire window
+                        if are_all_the_same(get_values(i, w, w - 1, 'trocou', col, values)):
+                            # if it did not change coach, should not change in the entire window
                             return True
                     else:
-                        # if changed, does not matter if changed again
+                        # if changed coach, does not matter if changed again
                         return True
     return False
 
 
-def get_values(i,mw,pw,name,col,values):
-    return [v[col[name]] for v in values[i-mw:i+pw+1]]
+def get_values(i, mw, pw, name, col, values):
+    return [v[col[name]] for v in values[i - mw:i + pw + 1]]
 
 
 def are_all_the_same(iterator):
-        return len(set(iterator)) <= 1
+    return len(set(iterator)) <= 1
 
 
-def write_csv(filename,coln, buffer):
+def write_csv(filename, coln, buffer):
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows([coln])
@@ -55,18 +54,18 @@ def remove_single(pairs):
     single = []
     k = 0
     while k < len(pairs):
-        if k == len(pairs)-1:
+        if k == len(pairs) - 1:
             # it is the last one
             single.append(k)
             k += 1
         else:
             # exists a later element
-            if pairs[k][cold['m']] == pairs[k+1][cold['m']] and pairs[k][cold['time']] == pairs[k+1][cold['time']]:
+            if pairs[k][cold['m']] == pairs[k + 1][cold['m']] and pairs[k][cold['time']] == pairs[k + 1][cold['time']]:
                 # double checking if trocou are different
                 if pairs[k][cold['trocou']] == pairs[k + 1][cold['trocou']]:
-                    print(w,k)
+                    print(w, k)
                     print(pairs[k])
-                    print(pairs[k+1])
+                    print(pairs[k + 1])
                     raise ValueError('The pair has the same value of trocou, the algorithm has to be improved.')
                 # it is married pair
                 k += 2
@@ -87,12 +86,12 @@ def getting_windowed_data(pairs):
     out = []
     for p in pairs:
         i = p[-1]
-        for k in range(i-w,i+w+1):
+        for k in range(i - w, i + w + 1):
             line = copy.deepcopy(values[k])
             # copying m to the entire window
             line[cold['m']] = values[i][cold['m']]
             # adding periodo
-            if k < i :
+            if k < i:
                 line[cold['periodo']] = 0
             elif k > i:
                 line[cold['periodo']] = 1
@@ -111,23 +110,21 @@ for w in range(1, 13):
     # getting valid pairs
     pairs = []
     for i in range(len(values)):
-        if is_valid_pair(i,w,l,cold,values):
-            pairs.append(values[i]+[i])
+        if is_valid_pair(i, w, l, cold, values):
+            pairs.append(values[i] + [i])
 
     # sorting by m and then by time
-    pairs = sorted(pairs,key=lambda x: x[cold['m']])
-    pairs = sorted(pairs,key=lambda x: x[cold['time']])
+    pairs = sorted(pairs, key=lambda x: x[cold['m']])
+    pairs = sorted(pairs, key=lambda x: x[cold['time']])
 
     # removing single pairs
     pairs = remove_single(pairs)
 
     # saving preliminary result for debug purposes
-    write_csv('{name:s}_pairs_window_{w:03d}.csv'.format(name=in_filename.split('.csv')[0],w=w), coln, pairs)
+    write_csv('{name:s}_pairs_window_{w:03d}.csv'.format(name=in_filename.split('.csv')[0], w=w), coln, pairs)
 
     # getting windowed data
     out = getting_windowed_data(pairs)
 
     # writing windowed data
-    write_csv('{name:s}_window_{w:03d}.csv'.format(name=in_filename.split('.csv')[0],w=w), coln, out)
-
-
+    write_csv('{name:s}_window_{w:03d}.csv'.format(name=in_filename.split('.csv')[0], w=w), coln, out)
